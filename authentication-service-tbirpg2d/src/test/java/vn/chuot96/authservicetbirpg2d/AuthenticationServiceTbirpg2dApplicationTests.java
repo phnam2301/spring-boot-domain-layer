70deda9eba75e1080c15af4c9f1bdf57eb6acbc7
William Phan <phnam230197@gmail.com>
2025-08-05 23:10:19 +0700

    repo
    add module authentication-service-tbirpg2d

---
+++ authentication-service-tbirpg2d/src/test/java/vn/chuot96/authservicetbirpg2d/AuthenticationServiceTbirpg2dApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.chuot96.authservicetbirpg2d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceTbirpg2dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
