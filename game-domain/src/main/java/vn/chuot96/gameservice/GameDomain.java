0185ac56bb7d0b91eaee48818c514b4add39322e
William Phan <phnam230197@gmail.com>
2025-08-11 00:25:19 +0700

    repo
    rename module game-service to 'game-domain'

--- game-domain/src/main/java/vn/chuot96/gameservice/GameDomain.java
+++ game-domain/src/main/java/vn/chuot96/gameservice/GameDomain.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GameService {
+public class GameDomain {
     public static void main(String[] args) {
-        SpringApplication.run(GameService.class, args);
+        SpringApplication.run(GameDomain.class, args);
     }
 }
