0b6a8a2f3ab1797e9e6ac848154674421249042a
William Phan <phnam230197@gmail.com>
2025-08-10 23:51:00 +0700

    repo
    remove other internal repo migrated modules

--- authentication-service/src/main/java/vn/chuot96/authservice/dto/RequestDTO.java
+++
@@ -1,4 +0,0 @@
-package vn.chuot96.authservice.dto;
-
-public record RequestDTO(String provider, String subject, String objectCode // Acc Credential or App Code
-        ) {}
--- authentication-service/src/main/java/vn/chuot96/authservice/dto/UserDTO.java
+++
@@ -1,5 +0,0 @@
-package vn.chuot96.authservice.dto;
-
-import java.util.List;
-
-public record UserDTO(String provider, String subject, String scope, List<String> audience) {}
